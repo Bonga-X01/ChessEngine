e7e1c8287cbd2086fcd5b676b0c22235d9fa8805
Bonga-X01 <bbonginkosi18@gmail.com>
2023-12-11 19:49:54 +0200

    Piece class

--- src/main/java/chess/Tile.java
+++ src/main/java/chess/Tile.java
@@ -7,4 +7,20 @@ public abstract class Tile {
     }
     public abstract boolean isTileOccupied();
     public abstract Piece getPiece();
+
+    public static final class EmptyTile extends Tile {
+        EmptyTile(int coordinate) {
+            super(coordinate);
+        }
+
+        @Override
+        public boolean isTileOccupied() {
+            return false;
+        }
+
+        @Override
+        public Piece getPiece() {
+            return null;
+        }
+    }
 }
