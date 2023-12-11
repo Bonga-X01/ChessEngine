30eca98dd1c4113c0085a6c9c0acf9fc0cd40e40
Bonga-X01 <bbonginkosi18@gmail.com>
2023-12-11 20:03:41 +0200

    Occupied tile

--- src/main/java/chess/Tile.java
+++ src/main/java/chess/Tile.java
@@ -23,4 +23,21 @@ public abstract class Tile {
             return null;
         }
     }
+    public static final class OccupiedTile extends Tile {
+        Piece pieceOnTile;
+        OccupiedTile(int coordinate, Piece pieceOnTile) {
+            super(coordinate);
+            this.pieceOnTile = pieceOnTile;
+        }
+
+        @Override
+        public boolean isTileOccupied() {
+            return true;
+        }
+
+        @Override
+        public Piece getPiece() {
+            return this.pieceOnTile;
+        }
+    }
 }
