4158e2508c0f578ec3c49667706cefc5213fe026
Bonga-X01 <bbonginkosi18@gmail.com>
2023-12-12 15:04:21 +0200

    Adding members to Piece class

--- src/main/java/chess/engile/pieces/Piece.java
+++ src/main/java/chess/engile/pieces/Piece.java
@@ -1,5 +1,17 @@
 package chess.engile.pieces;
 
-public class Piece {
+import java.util.List;
 
+public abstract class Piece {
+
+    protected final int piecePosition;
+    protected final Alliance pieceAlliance;
+
+    Piece(final int piecePosition, final Alliance pieceAlliance) {
+        this.piecePosition = piecePosition;
+        this.pieceAlliance = pieceAlliance;
+    }
+
+    public abstract List<Move> calculateLegalMoves(final Board board);
 }
+
--- src/main/java/chess/engine/board/Tile.java
+++ src/main/java/chess/engine/board/Tile.java
@@ -8,13 +8,19 @@ import java.util.Map;
 
 public abstract class Tile {
     protected final int tileCoordinates;
-    private static final Map<Integer, EmptyTile> EMPTY_TILES = createPossiblyEmptyTiles();
+    private static final Map<Integer, EmptyTile> EMPTY_TILES_CACHE = createPossiblyEmptyTiles();
 
+    /**
+     * This method creates an unmodifiable Map.
+     *
+     * @return The unmodifiable Map from Guava library.
+     */
     private static Map<Integer, EmptyTile> createPossiblyEmptyTiles() {
         final Map<Integer, EmptyTile> emptyTiles = new HashMap<>();
         for (int i = 0; i < 64; i++) {
             emptyTiles.put(i, new EmptyTile(i));
         }
+
         return ImmutableMap.copyOf(emptyTiles);
     }
 
@@ -23,13 +29,13 @@ public abstract class Tile {
     }
 
     public static Tile createTile(final int tileCoordinates, final Piece piece) {
-        return piece != null ? new EmptyTile(tileCoordinates) : EMPTY_TILES.get(tileCoordinates);
+        return piece != null ? new EmptyTile(tileCoordinates) : EMPTY_TILES_CACHE.get(tileCoordinates);
     }
     public abstract boolean isTileOccupied();
     public abstract Piece getPiece();
 
     public static final class EmptyTile extends Tile {
-        EmptyTile(final int coordinate) {
+        private EmptyTile(final int coordinate) {
             super(coordinate);
         }
 
@@ -45,7 +51,7 @@ public abstract class Tile {
     }
     public static final class OccupiedTile extends Tile {
         private final Piece pieceOnTile;
-        OccupiedTile(int coordinate, Piece pieceOnTile) {
+        private OccupiedTile(final int coordinate, final Piece pieceOnTile) {
             super(coordinate);
             this.pieceOnTile = pieceOnTile;
         }
